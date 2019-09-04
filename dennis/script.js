b8cc3fc3d79b04b5a8cb4d48d76763ca8f829093
Cedercrm <mattis.cederc@gmail.com>
2019-09-04 11:30:17 +0300

    Change title

--- dennis/script.js
+++ dennis/script.js
@@ -9,13 +9,23 @@ window.onload = function() {
             effectOff(this.id);
         });
         profiles[i].addEventListener("click",function(){
-            editTitle(this.id);
+            editTitle(this);
         });
     }
 }
-var editTitle = function(e) {
-    
-};
+
+function editTitle(elm) {
+    let title = document.getElementById(elm.id).parentElement.lastElementChild;
+    let newtitle = document.createElement("input");
+    newtitle.type = "text";
+    newtitle.placeholder = title.innerHTML;
+    newtitle.addEventListener("change", function(){
+        title.innerHTML = newtitle.value;
+        newtitle.remove();
+    });
+    elm.parentElement.appendChild(newtitle);
+    //title.remove();
+}
 
 function effectOn(id){
     document.getElementById(id).parentElement.classList.add("selected");
@@ -25,3 +35,6 @@ function effectOff(id){
     document.getElementById(id).parentElement.classList.remove("selected");
 }
 
+function changeTitle(){
+
+}
