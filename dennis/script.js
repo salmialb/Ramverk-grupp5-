c735b77bf8cd7633b5a912338ab0c751c524602f
Patrik Nyman <nymanpat@arcada.fi>
2019-09-04 11:23:19 +0300

    base for mouseOver

--- dennis/script.js
+++ dennis/script.js
@@ -1,14 +1,26 @@
 window.onload = function() {
     // Lyssna efter händelser
+    var queryResult = document.getElementsByClassName('team-member');
 }
+
+    Array.from(queryResult).forEach(function(element) {
+    var id = document.getElementById(element.id);
+    console.log(id);
+    document.getElementById(element.id).addEventListener("mouseover",effectOn(id));
+});
 var editTitle = function(e) {
     // Ändra "team-title"
 };
 
+
+
 function effectOn(id){
-    // Rita en ram runt personen, kanske lite drop shadow?
+    document.getElementById(id).style.border = "2px solid black";
+    alert(id);
 }
 
 function effectOff(id){
     // Stäng av effekten när musen inte längre är ovanför personen
 }
+
+
