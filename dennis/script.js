31bea1079c647298478e6e21a228ea2eae580e3e
Oscar Lytz <lytzdano@arcada.fi>
2019-09-04 10:40:58 +0300

    Dennis DOM challenge

---
+++ dennis/script.js
@@ -0,0 +1,14 @@
+window.onload = function() {
+    // Lyssna efter händelser
+}
+var editTitle = function(e) {
+    // Ändra "team-title"
+};
+
+function effectOn(id){
+    // Rita en ram runt personen, kanske lite drop shadow?
+}
+
+function effectOff(id){
+    // Stäng av effekten när musen inte längre är ovanför personen
+}
